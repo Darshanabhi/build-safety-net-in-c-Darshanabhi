4dec4ff58a72f410e2e022de5dd7987f9b5ab5ae
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-13 11:47:20 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,1 +1,11 @@
 
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
+
+#include "Soundex.h"
+#include <ctype.h>
+#include <string.h>
+
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+}
