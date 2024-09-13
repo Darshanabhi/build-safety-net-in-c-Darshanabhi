0959d81ba38dbfcca4eef52210147558907de016
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-13 11:52:23 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,4 +1,9 @@
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    soundex[0] = toupper(name[0]);
+}
+
 
