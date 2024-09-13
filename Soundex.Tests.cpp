5d777d0fea37f61aa12d450bae0ddd5551201f08
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-13 11:38:43 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,1 +1,8 @@
+#include <gtest/gtest.h>
+#include "Soundex.h"
 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+  char soundex[5];   
+  generateSoundex("", soundex);
+  EXPECT_EQ(soundex,"");
+}
